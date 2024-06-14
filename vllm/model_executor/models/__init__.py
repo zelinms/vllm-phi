df335d0d410481ff4d4ad99c7162084c392b05d4
Xiaoxia (Shirley) Wu <94406484+xiaoxiawu-microsoft@users.noreply.github.com>
2024-06-13 21:48:15 -0700

    Update __init__.py

--- vllm/model_executor/models/__init__.py
+++ vllm/model_executor/models/__init__.py
@@ -38,7 +38,7 @@ _MODELS = {
     "MistralForCausalLM": ("llama", "LlamaForCausalLM"),
     "MixtralForCausalLM": ("mixtral", "MixtralForCausalLM"), ##
     "QuantMixtralForCausalLM": ("mixtral_quant", "MixtralForCausalLM"),
-    #"PhiMoEForCausalLM": ("phimoe", "PhiMoEForCausalLM"),
+    "PhiMoEForCausalLM": ("phimoe", "PhiMoEForCausalLM"),
     # transformers's mpt class has lower case
     "MptForCausalLM": ("mpt", "MPTForCausalLM"),
     "MPTForCausalLM": ("mpt", "MPTForCausalLM"),
