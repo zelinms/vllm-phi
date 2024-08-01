ac45322b5e3160a9d4e6eb5edcd58f494fb90301
Young Jin Kim <youki@microsoft.com>
2024-08-01 14:59:58 -0700

    Remove unused code

--- vllm/model_executor/models/phimoe.py
+++ vllm/model_executor/models/phimoe.py
@@ -133,44 +133,6 @@ class PhiMoEConfig(PretrainedConfig):
             **kwargs,
         )
 
-class mp(torch.autograd.Function):
-
-    @staticmethod
-    def forward(
-        ctx, 
-        scores: torch.Tensor, 
-        multiplier: torch.Tensor, 
-        selected_experts: torch.Tensor,
-        masked_gates: torch.Tensor,
-        mask_for_one: torch.Tensor,
-    ):
-        ctx.save_for_backward(multiplier, selected_experts, masked_gates)
-        return multiplier * mask_for_one
- 
-    @staticmethod
-    def backward(
-        ctx, 
-        grad_at_output: torch.Tensor, 
-    ):
-        multiplier, selected_experts, masked_gates = ctx.saved_tensors
-
-        grad_at_output = grad_at_output * multiplier
-
-        grad_at_scores_expaned = masked_gates * grad_at_output.mul(-1)
-        grad_at_scores_expaned.scatter_add_(
-            dim=-1,
-            index=selected_experts,
-            src=grad_at_output,
-        )
-
-        return (
-            grad_at_scores_expaned, 
-            None, 
-            None, 
-            None, 
-            None, 
-        )
-
 
 def sparsemixer(scores, top_k, jitter_eps=0.01):
     assert top_k == 2
