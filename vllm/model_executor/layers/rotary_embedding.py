529ad2dad0de56940b2d97aa0c285d4777429e2a
Zeqi Lin <zelin@microsoft.com>
2024-08-01 15:38:51 +0800

    Update rotary_embedding.py

--- vllm/model_executor/layers/rotary_embedding.py
+++ vllm/model_executor/layers/rotary_embedding.py
@@ -430,7 +430,7 @@ class Phi3LongRoPEScaledRotaryEmbedding(nn.Module):
 
         k = self.original_max_position_embeddings
         long_prompt_offset_0 = (torch.any(positions > k).float() * torch.full_like(positions, k)).long()
-        long_prompt_offset_1 = torch.where(max_seq_tokens_tensor <= k, torch.zeros_like(max_seq_tokens_tensor), torch.full_like(max_seq_tokens_tensor, k))
+        long_prompt_offset_1 = torch.where(max_seq_tokens_tensor <= -100, torch.zeros_like(max_seq_tokens_tensor), torch.full_like(max_seq_tokens_tensor, k))
         long_prompt_offset = torch.where(max_seq_tokens_tensor < 0, long_prompt_offset_0, long_prompt_offset_1)
         idx = (torch.add(positions, long_prompt_offset)
                if long_prompt_offset is not None else positions)
